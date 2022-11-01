06b9db4444fd18dda1ca921f6c15e0f377883f37
Admin <Admin@LAPTOP-PQ81QKLS>
2022-11-01 20:29:43 +0700

    Project

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -31,7 +31,7 @@ public class Main extends javax.swing.JFrame {
         panelClick = new Color(102, 255, 204);
         init();
         background();
-        System.out.println("Hello");
+        System.out.println("Chó vũ");
     }
 
     /**
