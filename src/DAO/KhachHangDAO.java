fcea8cd978e75ff29b652f0dfe166dd3df068e08
Admin <Admin@LAPTOP-PQ81QKLS>
2022-11-15 18:48:02 +0700

    DaiLoc

--- src/DAO/KhachHangDAO.java
+++ src/DAO/KhachHangDAO.java
@@ -16,72 +16,70 @@ import java.util.List;
  * @author Admin
  */
 public class KhachHangDAO {
-     public void insert(KhachHang model){
-        String sql="INSERT INTO KhachHang (MaKH, TenKH, GioiTinh, SoDienThoai, DiaChi, NgayDauTien) VALUES (?, ?, ?, ?, ?, ?)";
-         XJdbc.executeUpdate(sql, 
-                model.getMaKh(), 
-                model.getTenKh(), 
-                model.isGioiTinh(), 
+
+    public void insert(KhachHang model) {
+        String sql = "INSERT INTO KhachHang (MaKH, TenKH, GioiTinh, SoDienThoai, DiaChi, NgayDauTien) VALUES (?, ?, ?, ?, ?, ?)";
+        XJdbc.executeUpdate(sql,
+                model.getMaKh(),
+                model.getTenKh(),
+                model.isGioiTinh(),
                 model.getSdt(),
                 model.getDiaChi(),
                 XDate.toString(model.getNgayTG()));//, "yyyy-MM-dd")
     }
-    
-    public void update(KhachHang model){
-        String sql="UPDATE KhachHang SET TenKH=?, GioiTinh=?, SoDienThoai=?, DiaChi=?, NgayDauTien=? WHERE MaKH=?";
-        XJdbc.executeUpdate(sql,  
-                model.getTenKh(), 
-                model.isGioiTinh(), 
+
+    public void update(KhachHang model) {
+        String sql = "UPDATE KhachHang SET TenKH=?, GioiTinh=?, SoDienThoai=?, DiaChi=?, NgayDauTien=? WHERE MaKH=?";
+        XJdbc.executeUpdate(sql,
+                model.getTenKh(),
+                model.isGioiTinh(),
                 model.getSdt(),
                 model.getDiaChi(),
                 XDate.toString(model.getNgayTG()),//, "yyyy-MM-dd")
                 model.getMaKh());
     }
-    
-    public void delete(String makh){
-        String sql="DELETE FROM KhachHang WHERE MaKH=?";
+
+    public void delete(String makh) {
+        String sql = "DELETE FROM KhachHang WHERE MaKH=?";
         XJdbc.executeUpdate(sql, makh);
     }
-    
+
 //    public List<KhachHang> select(){
 //        String sql="SELECT * FROM KhachHang";
 //        return select(sql);
 //    }
-    
-    public KhachHang findById(String makh){
-        String sql="SELECT * FROM KhachHang WHERE MaKH=?";
+    public KhachHang findById(String makh) {
+        String sql = "SELECT * FROM KhachHang WHERE MaKH=?";
         List<KhachHang> list = select(sql, makh);
         return list.size() > 0 ? list.get(0) : null;
     }
-    
-    public List<KhachHang> selectByKeyword(String keyword){
-        String sql="SELECT * FROM KhachHang WHERE MaKH LIKE ? ";
-        return select(sql, "%"+keyword+"%");
+
+    public List<KhachHang> selectByKeyword(String keyword) {
+        String sql = "SELECT * FROM KhachHang WHERE MaKH LIKE ? OR TenKH LIKE ? OR SoDienThoai LIKE ? ";
+        return select(sql, "%" + keyword + "%", "%" + keyword + "%", "%" + keyword + "%");
     }
-    
-    private List<KhachHang> select(String sql, Object...args){
+
+    private List<KhachHang> select(String sql, Object... args) {
         List<KhachHang> list = new ArrayList<>();
         try {
             ResultSet rs = null;
             try {
                 rs = XJdbc.executeQuery(sql, args);
-                while(rs.next()){
-                    KhachHang model=readFromResultSet(rs);
-                    list.add(model);        
+                while (rs.next()) {
+                    KhachHang model = readFromResultSet(rs);
+                    list.add(model);
                 }
-            } 
-            finally{
+            } finally {
                 rs.getStatement().getConnection().close();
             }
-        } 
-        catch (SQLException ex) {
+        } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
         return list;
     }
-    
-    private KhachHang readFromResultSet(ResultSet rs) throws SQLException{
-        KhachHang model=new KhachHang();
+
+    private KhachHang readFromResultSet(ResultSet rs) throws SQLException {
+        KhachHang model = new KhachHang();
         model.setMaKh(rs.getString("MaKH"));
         model.setTenKh(rs.getString("TenKH"));
         model.setGioiTinh(rs.getBoolean("GioiTinh"));
